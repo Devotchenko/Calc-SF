53ed7b3326ec0cc9e81c12b5ae0ae2d0b7baa2cc
Evel Knievel <dr_ju@mail.ru>
2023-10-18 16:57:20 +0300

    Добавьте файлы проекта.

---
+++ Calc SF/Calc SF.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int x = 0;
+	int y = 0;
+	float z = 0.0;
+	char evalutionOperator;
+	std::cout << "введите первое число:\n";
+	std::cin >> x;
+	if (!std::cin) {
+		std::cout << "неправильный ввод\n";
+		return 1;
+	}
+	std::cout << "введите второе число:\n";
+	std::cin >> y;
+	if (!std::cin) {
+		std::cout << "неправильный ввод\n";
+		return 1;
+	}
+	std::cout << "введите оператор вычисления (+,-,*,/):\n";
+	std::cin >> evalutionOperator;
+	if (!std::cin) {
+		std::cout << "неправильный ввод\n";
+		return 1;
+	}
+
+	switch (evalutionOperator)
+	{
+	case '+':
+		z = x + y;
+		std::cout << "сложение чисел  " << x << " и " << y << " дает ответ " << z; "\n";
+		break;
+	case '-':
+		z = x - y;
+		std::cout << "вычитание   " << y << " из " << x << " дает ответ " << z; "\n";
+		break;
+	case '*':
+		z = x * y;
+		std::cout << "умножение   " << x << " на " << y << " дает ответ " << z; "\n";
+		break;
+	case '&':
+		z = x & y;
+		std::cout << "результат побитового AND   " << x << " на " << y << " дает ответ " << z; "\n";
+		break;
+	case '^':
+		z = x ^ y;
+		std::cout << "результат побитового XOR   " << x << " на " << y << " дает ответ " << z; "\n";
+		break;
+	case '|':
+		z = x | y;
+		std::cout << "результат побитового OR   " << x << " на " << y << " дает ответ " << z; "\n";
+		break;
+	case '/':
+		if (y == 0) {
+			std::cout << "на ноль делить нельзя\n";
+			return 2;
+		}
+		z = static_cast<float>(x) / static_cast<float>(y);
+		std::cout << "деление  " << x << " на " << y << " дает ответ " << z; "\n";
+		break;
+	default:
+		std::cout << evalutionOperator << " - некорректный ввод! ";
+		std::cout << "Сделайте правильный выбор (+,-,*,/): ";
+	}
+
+
+
+	return 0;
+}
